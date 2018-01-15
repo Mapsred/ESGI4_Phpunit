f980912934fbc8431ba94e97371092bcfa32652e
François MATHIEU <francois.mathieu@livexp.fr>
2018-01-15 19:13:31 +0100

    Fix selenium/behat for Chrome

--- features/Context/FeatureContext.php
+++ features/Context/FeatureContext.php
@@ -2,7 +2,6 @@
 
 namespace Context;
 
-use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Behat\Context\Context;
 use Behat\MinkExtension\Context\MinkContext;
 
@@ -11,22 +10,4 @@ use Behat\MinkExtension\Context\MinkContext;
  */
 class FeatureContext extends MinkContext implements Context
 {
-    /**
-     * Initializes context.
-     *
-     * Every scenario gets its own context instance.
-     * You can also pass arbitrary arguments to the
-     * context constructor through behat.yml.
-     */
-    public function __construct()
-    {
-    }
-
-    /**
-     * @When I wait for the suggestion box to appear
-     */
-    public function iWaitForTheSuggestionBoxToAppear()
-    {
-        throw new PendingException();
-    }
 }
